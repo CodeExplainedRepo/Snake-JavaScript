be20834062339f6a87d7a415a51caa44f1df7512
vincentinttsh <vincentinttsh@gmail.com>
2019-10-19 01:01:05 +0800

    1.0

---
+++ HW4.js
@@ -0,0 +1,111 @@
+/*
+Create by Learn Web Developement
+Youtube channel : https://www.youtube.com/channel/UC8n8ftV94ZU_DJLOLtrpORA
+*/
+
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// create the unit
+const box = 32;
+
+// load images
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+// load audio files
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
+
+// create the snake
+
+let snake = [];
+
+snake[0] = {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
+};
+
+// create the score var
+
+let score = 0;
+
+//control the snake
+
+box_x = -1;
+box_y = -1;
+
+// draw everything to the canvas
+
+function draw() {
+
+    ctx.drawImage(ground, 0, 0);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = (i == 0) ? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // move
+    snakeY += box * box_y
+    snakeX += box * box_x;
+
+    // add new Head
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    // change direction
+    let change = false;
+
+    if (snakeX < box) {
+        box_x *= -1;
+        score += 5;
+        change = true;
+    }
+    if (snakeX > 17 * box) {
+        box_x *= -1;
+        score += 5;
+        change = true;
+    }
+    if (snakeY < 3 * box) {
+        box_y *= -1;
+        score += 5;
+        change = true;
+    }
+    if (snakeY > 17 * box) {
+        box_y *= -1;
+        score += 5;
+        change = true;
+    }
+
+    if (!change)
+        snake.pop()
+
+    snake.unshift(newHead);
+
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score, 2 * box, 1.6 * box);
+}
+
+// call draw function every 100 ms
+
+let game = setInterval(draw, 100);
---
+++ HW5.js
@@ -0,0 +1,146 @@
+/*
+Create by Learn Web Developement
+Youtube channel : https://www.youtube.com/channel/UC8n8ftV94ZU_DJLOLtrpORA
+*/
+
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// create the unit
+const box = 32;
+
+// load images
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// load audio files
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
+
+// create the snake
+
+let snake = [];
+
+snake[0] = {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
+};
+
+// create the food
+
+let food = {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
+}
+
+// create the score var
+
+let score = 0;
+
+//control the snake
+
+box_x = -1;
+box_y = -1;
+
+//control the food
+
+document.addEventListener("keydown", direction);
+
+function direction(event) {
+    let key = event.keyCode;
+    if (key == 37 && food.x > box) {
+        food.x -= box
+    } else if (key == 38 && food.y > 3 * box) {
+        food.y -= box
+    } else if (key == 39 && food.x < 17 * box) {
+        food.x += box;
+    } else if (key == 40 && food.y < 17 * box) {
+        food.y += box;
+    }
+}
+
+// check eat function
+function Eat_Food(food, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (food.x == array[i].x && food.y == array[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// draw everything to the canvas
+
+function draw() {
+
+    ctx.drawImage(ground, 0, 0);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // move
+    snakeY += box * box_y
+    snakeX += box * box_x;
+
+    // add new Head
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    // change direction
+
+    if (snakeX < box) box_x *= -1;
+    if (snakeX > 17 * box) box_x *= -1;
+    if (snakeY < 3 * box) box_y *= -1;
+    if (snakeY > 17 * box) box_y *= -1;
+
+    // if the snake eats the food
+    if (Eat_Food(food, snake)) {
+        score += 5;
+        eat.play();
+        food = {
+            x: Math.floor(Math.random() * 17 + 1) * box,
+            y: Math.floor(Math.random() * 15 + 3) * box
+        }
+        // we don't remove the tail
+    } else {
+        // remove the tail
+        snake.pop();
+    }
+
+    snake.unshift(newHead);
+
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score, 2 * box, 1.6 * box);
+}
+
+// call draw function every 100 ms
+
+let game = setInterval(draw, 100);
